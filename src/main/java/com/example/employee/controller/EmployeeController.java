f997255837972bdf6c191b942693d6d1baa1b6e8
MerguMayuri <MerguMayuri@github.com>
2025-05-29 20:42:51 +0530

    Initial commit - Spring Boot Employee Management System

---
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.employee.controller;
+
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
