7ca4822e466353ab5edd1189fb50f1a451118c9a
MerguMayuri <MerguMayuri@github.com>
2025-06-02 21:39:44 +0530

    Added Map and Hashmap import in controller.java file

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+// Added below imports as @DeleteMapping needed those imports to execute.
+import org.springframework.http.ResponseEntity;
+import java.util.Map;
+import java.util.HashMap;
+
 
 @RestController
 @RequestMapping("/api/employees")
